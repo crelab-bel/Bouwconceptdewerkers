786a25e76390b227f47a2079a4de9fb181a47501
crelab-bel <168651978+crelab-bel@users.noreply.github.com>
2024-05-04 17:53:41 +0200

    Add Base64 encoded file

--- app/content.js
+++ app/content.js
@@ -1,4 +1,4 @@
 export const mainHero = {
-    h1: 'Expert Dakwerken en Isolatie in Zonhoven',
-    paragraaf: 'Op zoek naar betrouwbare dakrenovatie of isolatiewerken? Ontdek onze professionele service en duurzame oplossingen op maat in Zonhoven.',
+    h1: 'Expert Dakwerken en Isolatie Diensten',
+    paragraaf: 'Verhoog de waarde en energie-efficiëntie van uw woning met onze professionele dakrenovatie, reparatie en isolatieservices. Betrouwbaar, duurzaam en op maat gemaakt voor elk huis.',
 };
